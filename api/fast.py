f178cd5ef87b485eff9fbb7f9da9b6b725cfaff6
Moritz Geiger <moritzgeiger@hotmail.de>
2021-02-25 18:35:17 +0100

    finishing up

--- api/fast.py
+++ api/fast.py
@@ -24,12 +24,12 @@ def predict_fare(pickup_datetime, pickup_longitude, pickup_latitude, dropoff_lon
     pipeline = joblib.load(PATH_TO_LOCAL_MODEL)
     X_pred = pd.DataFrame({
                            "key": '2012-10-06 12:10:20.0000001',
-                           "pickup_datetime": [pickup_datetime],
-                           "pickup_longitude": [pickup_longitude],
-                           "pickup_latitude": [pickup_latitude], 
-                           "dropoff_longitude": [dropoff_longitude], 
-                           "dropoff_latitude": [dropoff_latitude], 
-                            "passenger_count": [dropoff_latitude]
+                           "pickup_datetime": [float(pickup_datetime)],
+                           "pickup_longitude": [float(pickup_longitude)],
+                           "pickup_latitude": [float(pickup_latitude)], 
+                           "dropoff_longitude": float(dropoff_longitude)], 
+                           "dropoff_latitude": [float(dropoff_latitude)], 
+                            "passenger_count": [int(passenger_count)]
                             })
     y_pred = pipeline.predict(X_pred)[0]
     return {
